30bf4bd3a73604c9663e637113937cebd0a48399
Henrik Aldén <121222619+hen-ald@users.noreply.github.com>
2023-11-09 10:07:42 +0100

    Add files via upload

---
+++ deso_dalarna_kom.R
@@ -0,0 +1,24 @@
+
+# libraries
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(tidyverse, sf, sp, httr, mapview, leaflet)
+
+
+lan_kod = "20"
+
+deso_fil <- "DeSO_2018_v2.gpkg"
+
+deso = st_read(deso_fil)
+
+deso = filter(deso, lan == lan_kod)
+
+mapview(deso)
+#---------------------------------------------------------------------------------------------------
+# Create kommun boundaries based on DeSo boundaries 
+#---------------------------------------------------------------------------------------------------
+kommun = deso %>% 
+  group_by(kommun, kommunnamn) %>% 
+  summarize(geom = st_union(geom)) %>% 
+  ungroup()
+
+mapview(kommun)
